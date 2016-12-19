4c5f368d4d3f22bd9c01034f422b8d0188ada854
Xavier <qazxiaye@126.com>
2016-12-19 20:04:52 +0100

    add parameter support; add file type verification.

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-This tool reverses colors of all images which are under working directory of the executable or under a directory given in the source. There should be no other file types under the directory. Generated files will be found in folder "gen" which will be under the aforementioned directory. Generated files' name will be the same as original images.
+This tool is for color reversing of a batch of images. Generated files will be found in folder "gen" under working directory. Reversed files' name will be the same as their source image.
 
-Usage: two possibilities
+Usage: 
 
- 1. change the value of static variable "path" in source code as the given directory mentioned above, then run it anywhere.
- 2. set the value of static variable "path" as *System.getProperty("user.dir")*. Export runnable jar (which can be found in this respectory, ReverseColor.jar) and move this file to the desired directory containing images, then run. Note: There'll be exceptions launching directly in IDE in this case, due to other file types under the working directory.
+ - if no parameter has been passed, all images under working directory will be source image to be reversed.
 
-<br><br>
+ - otherwise, all images in parameters will be source image to be reversed.
 
-这是一个将文件夹下所有图片颜色反转的小工具。可以设置具体路径指定文件夹，也可设置为科执行文件的当前工作路径。反色图片生成在路径下的新文件夹“gen”中，生成图片与原文件同名。
 
-使用：
- 
- 1. 选项1：修改源码中变量 "path" 指定文件夹路径。
- 2. 选项2：将 "path" 的值设为 *System.getProperty("user.dir")*，导出 runnable jar，将导出文件移至相应文件夹，并运行。
+<br>
+ - original image
+
+ ![](NetTopology.ppm.gif)
+
+ - reversed image
+
+ ![](gen/NetTopology.ppm.gif)
+
--- src/ReverseColor.java
+++ src/ReverseColor.java
@@ -6,13 +6,13 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.imageio.ImageIO;
 
 class ReverseColor
 {
-	static String path = "~/test";
-//	static String path = System.getProperty("user.dir");
+	static String currDir = System.getProperty("user.dir");
 	
 	static void GenReverseImg(File file)
 	{
@@ -25,6 +25,11 @@ class ReverseColor
 		{
 			e.printStackTrace();
 		}
+		
+		if(image == null)
+			return;
+		
+		System.out.println("Reversing color of image " + file.getName());
 		
 		for (int x = 0; x < image.getWidth(); x++)
 		{
@@ -43,32 +48,65 @@ class ReverseColor
 		
 		try
 		{
-			ImageIO.write(image, "PNG", new File(path + "/gen/" + file.getName()));
+			ImageIO.write(image, "PNG", new File(currDir + "/gen/" + file.getName()));
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
+		
+		System.out.println("Finished!");
 	}
 	
 	
 	public static void main(String args[])
 	{
 		//create "gen" folder
-		File genFolder = new File(path + "/gen");
+		File genFolder = new File(currDir + "/gen");
 		if (!genFolder.exists())
 		{
 			genFolder.mkdir();
 		}
 		
 		//generate images
-		File folder = new File(path);
-		File[] listOfFiles = folder.listFiles();
-		
-		for (File img : listOfFiles)
+		if(args.length == 0)
 		{
-			if (img.isFile())
-				GenReverseImg(img);
+			File folder = new File(currDir);
+			File[] listOfFiles = folder.listFiles();
+			
+			for (File img : listOfFiles)
+			{
+				try
+				{
+					String mimetype = Files.probeContentType(img.toPath());
+					
+					if (mimetype != null && mimetype.split("/")[0].equals("image"))
+						GenReverseImg(img);
+				} 
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		else
+		{
+			for(String path : args)
+			{
+				File img = new File(path);
+				
+				try
+				{
+					String mimetype = Files.probeContentType(img.toPath());
+					
+					if (mimetype != null && mimetype.split("/")[0].equals("image"))
+						GenReverseImg(img);
+				} 
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
